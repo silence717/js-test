1345d9e07695a2759680bd4ecfe718e3fc9a97f5
silence <silence_yfang@126.com>
2016-04-12 15:17:38 +0800

    ES5测试类更改

--- src/length/_test_/length-test.js
+++ src/length/_test_/length-test.js
@@ -1,29 +1,46 @@
-// /**
-//  * @description [测试长度的类库]
-//  * @author [silence]
-//  * @date  16/4/8
-//  */
-//
-// import Length from '../length.js';
-//
-// // 相等性测试
-// describe('isEqual', function () {
-//     // 相同单位
-//     it('compare with same unit', function () {
-//
-//         var length1 = {number: 1, unit: 'cm'};
-//         var length2 = {number:1, unit: 'cm'};
-//         expect(Length.isEqual(length1, length2)).toBeTruthy();
-//     });
-//     // 不同单位
-//     it('compare with different unit', function () {
-//         var length1 = {number: 1, unit: 'cm'};
-//         var length2 = {number: 1, unit: 'mm'};
-//         expect(Length.isEqual(length1, length2)).not.toBeTruthy();
-//     });
-// });
-//
-// // 相加性测试
+/**
+ * @description [测试长度的类库]
+ * @author [silence]
+ * @date  16/4/8
+ */
+
+import Length from '../length.js';
+
+// 相等性测试
+describe('length equals', ()=> {
+
+    it('One mm should be equal One mm ', () => {
+        let lenA = new Length(1, 'mm');
+        let lenB = new Length(1, 'mm');
+        expect(lenA.equals(lenB)).toBeTruthy();
+    });
+
+    it('One mm should not be equal Two mm', () => {
+        let lenA = new Length(1, 'mm');
+        let lenB = new Length(2, 'mm');
+        expect(lenA.equals(lenB)).toBeFalsy();
+    });
+
+    it('One cm should be equal Ten mm', () => {
+        let lenA = new Length(1, 'cm');
+        let lenB = new Length(10, 'mm');
+        expect(lenA.equals(lenB)).toBeTruthy();
+    });
+
+    it('One cm should not be equal Two mm', () => {
+        let lenA = new Length(1, 'cm');
+        let lenB = new Length(2, 'mm');
+        expect(lenA.equals(lenB)).toBeFalsy();
+    });
+
+    it('One m should be equal One Thousand mm', () =>{
+        let lenA = new Length(1, 'm');
+        let lenB = new Length(1000, 'mm');
+        expect(lenA.equals(lenB)).toBeTruthy();
+    });
+});
+
+// 相加性测试
 // describe('add', function () {
 //
 //     it('add the same  unit', function () {
@@ -39,8 +56,8 @@
 //     });
 //
 // });
-//
-// // 相减性测试
+
+// 相减性测试
 // describe('sub', function () {
 //
 //     it('sub the same unit', function () {
--- src/length/length.js
+++ src/length/length.js
@@ -3,65 +3,31 @@
  * @author [silence]
  * @date  16/4/8
  */
-var Length = {
+function Length(len, unit) {
+    this.len = len;
+    this.unit = unit;
+}
 
-    /**
-     * 判断是否相等
-     * @param obj1
-     * @param obj2
-     */
-    isEqual: function (obj1, obj2) {
-        var _obj1 = this.convertUnit(obj1);
-        var _obj2 = this.convertUnit(obj2);
-        return obj1.number == obj2.number ? true : false;
-    },
-    /**
-     * 长度相加
-     * @param obj1
-     * @param obj2
-     */
-    add: function (obj1, obj2) {
-        var obj1 = this.convertUnit(obj1);
-        var obj2 = this.convertUnit(obj2);
-        return obj1.number + obj2.number;
-    },
+Length.prototype.toString = function () {
+    return this.len + this.unit;
+};
 
-    /**
-     * 长度相减
-     * @param obj1
-     * @param obj2
-     */
-    sub: function (obj1, obj2) {
-        var obj1 = this.convertUnit(obj1);
-        var obj2 = this.convertUnit(obj2);
-        return obj1.number - obj2.number;
-    },
-
-    /**
-     * 需要转换单位的对象
-     */
-    convertUnit: function (object) {
-        var number = object.number;
-        var unit = object.unit;
-
-        switch(object.unit) {
-            case 'm':
-                number *= 1000;
-                break;
-            case 'cm':
-                number *= 10;
-                break;
-            case 'mm':
-                number *= 1;
-                break;
-            default:
-                number *= 1;
-                break;
-        }
-        object.number = number;
-        object.unit = 'mm';
-        return object;
+Length.prototype.toMM = function (len) {
+    var length = len.len;
+    if (len.unit == 'mm') {
+        length *= 1;
     }
+    if (len.unit == 'cm') {
+        length *= 10;
+    }
+    if (len.unit == 'm') {
+        length *= 1000;
+    }
+    return length;
+};
+
+Length.prototype.equals = function (that) {
+    return this.toMM(this) == this.toMM(that);
 };
 
 module.exports = Length;
